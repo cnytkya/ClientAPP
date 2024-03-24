4268b52347dc9c7eb734b4f3f12b64b001e56bf1
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2024-03-24 12:50:36 +0300

    Added Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent {
    { id:3,name:"Iphone s18", price:104213,isActive:true},
    { id:4,name:"Iphone s18", price:104213,isActive:false},
    { id:5,name:"Iphone s18", price:104213,isActive:true},
-   { id:6,name:"Iphone s18", price:104213,isActive:true}
+   { id:6,name:"Iphone s18", price:104213,isActive:true},
+   { id:6,name:"Iphone s19", price:104213,isActive:true}
   ]
 }
