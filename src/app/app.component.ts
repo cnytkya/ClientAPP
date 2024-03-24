eebad78f63f13c8622c8db10c0b75ada8a6f1abc
cnytkya <cuneytkaya73232542@gmail.com>
2024-03-24 12:03:43 +0300

    Added TelephoneCategory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ClientApp';
+  categoryName="Telefon Kategorisi"
+
+  products =[
+   { id:1,name:"Iphone s18", price:104213,isActive:true},
+   { id:2,name:"Iphone s18", price:104213,isActive:false},
+   { id:3,name:"Iphone s18", price:104213,isActive:true},
+   { id:4,name:"Iphone s18", price:104213,isActive:false},
+   { id:5,name:"Iphone s18", price:104213,isActive:true},
+   { id:6,name:"Iphone s18", price:104213,isActive:true}
+  ]
 }
